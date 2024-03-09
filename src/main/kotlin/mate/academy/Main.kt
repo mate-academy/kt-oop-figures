1a24772221d850551808e73607213da887c72310
Danil Ponomarenko <danil.ponomvrenko@gmail.com>
2024-03-09 16:08:02 +0200

    Added logic into `main()` due to our task description

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,5 +1,20 @@
 package mate.academy
 
+import mate.academy.service.FigureSupplier
+
+const val ARRAY_SIZE = 6
+
 fun main() {
-    // draw all figures here
+    val array = Array(ARRAY_SIZE) { FigureSupplier().getDefaultFigure() }
+    for (i in 0 until array.size - 1) {
+        if (i < array.size / 2) {
+            array[i] = FigureSupplier().getRandomFigure()
+        } else {
+            array[i] = FigureSupplier().getDefaultFigure()
+        }
+    }
+
+    for ((index, figure) in array.withIndex()) {
+        println("Figure $index: ${figure.getDescription()}")
+    }
 }
