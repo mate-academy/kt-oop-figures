09fcb403e19e78deecf6ba1021f83e28488712dc
Danil Ponomarenko <akame.dante13@gmail.com>
2024-03-11 17:04:09 +0200

    Created enum `FigureType` that helps with code optimization for readability and performance

--- src/main/kotlin/mate/academy/service/FigureSupplier.kt
+++ src/main/kotlin/mate/academy/service/FigureSupplier.kt
@@ -2,6 +2,7 @@ package mate.academy.service
 
 import mate.academy.model.Color
 import mate.academy.model.Figure
+import mate.academy.model.FigureType
 import mate.academy.model.figure.Circle
 import mate.academy.model.figure.IsoscelesTrapezoid
 import mate.academy.model.figure.Rectangle
@@ -9,49 +10,32 @@ import mate.academy.model.figure.RightTriangle
 import mate.academy.model.figure.Square
 import kotlin.random.Random
 
-const val TOTAL_FIGURES_QUANTITY = 5
 const val BOUND_FOR_RANDOM = 1000
 const val DEFAULT_SIZE = 10
-const val SQUARE_INDEX = 0
-const val CIRCLE_INDEX = 1
-const val TRAPEZOID_INDEX = 2
-const val RECTANGLE_INDEX = 3
-const val RIGHT_TRIANGLE_INDEX = 4
 
 class FigureSupplier {
 
-/*
-    Question for mentors: Is it better to declare const's like this??
-
-    companion object {
-        const val TOTAL_FIGURES_QUANTITY = 5
-        const val BOUND_FOR_RANDOM = 1000
-        const val DEFAULT_SIZE = 10
-    }
-*/
-
     fun getRandomFigure(): Figure {
         val color = ColorSupplier().getRandomColor()
-        return when (Random.nextInt(TOTAL_FIGURES_QUANTITY)) {
-            SQUARE_INDEX -> Square(Random.nextInt(BOUND_FOR_RANDOM), color)
-            CIRCLE_INDEX -> Circle(Random.nextInt(BOUND_FOR_RANDOM), color)
-            TRAPEZOID_INDEX -> IsoscelesTrapezoid(
+        return when (FigureType.values().random()) {
+            FigureType.SQUARE -> Square(Random.nextInt(BOUND_FOR_RANDOM), color)
+            FigureType.CIRCLE -> Circle(Random.nextInt(BOUND_FOR_RANDOM), color)
+            FigureType.TRAPEZOID -> IsoscelesTrapezoid(
                 Random.nextInt(BOUND_FOR_RANDOM),
                 Random.nextInt(BOUND_FOR_RANDOM),
                 Random.nextInt(BOUND_FOR_RANDOM),
                 color
             )
-            RECTANGLE_INDEX -> Rectangle(
+            FigureType.RECTANGLE -> Rectangle(
                 Random.nextInt(BOUND_FOR_RANDOM),
                 Random.nextInt(BOUND_FOR_RANDOM),
                 color
             )
-            RIGHT_TRIANGLE_INDEX -> RightTriangle(
+            FigureType.RIGHT_TRIANGLE -> RightTriangle(
                 Random.nextInt(BOUND_FOR_RANDOM),
                 Random.nextInt(BOUND_FOR_RANDOM),
                 color
             )
-            else -> throw IllegalArgumentException("Invalid figure type!")
         }
     }
 
