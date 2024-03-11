06f89e04eca0fbc94571b7e86263a6113c959213
Danil Ponomarenko <akame.dante13@gmail.com>
2024-03-11 17:02:05 +0200

    Optimized array initialization and output loop with `forEachIndexed()`

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -6,15 +6,13 @@ const val ARRAY_SIZE = 6
 
 fun main() {
     val array = Array(ARRAY_SIZE) { FigureSupplier().getDefaultFigure() }
-    for (i in 0 until array.size - 1) {
+    for (i in 0 until array.lastIndex) {
         if (i < array.size / 2) {
             array[i] = FigureSupplier().getRandomFigure()
-        } else {
-            array[i] = FigureSupplier().getDefaultFigure()
         }
     }
 
-    for ((index, figure) in array.withIndex()) {
-        println("Figure $index: ${figure.getDescription()}")
+    array.forEachIndexed {
+        index, figure -> println("Figure ${index + 1}: ${figure.getDescription()}")
     }
 }
