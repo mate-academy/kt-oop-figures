342f39ac96c5bea2cc45757388ffadeb61d96736
Danil Ponomarenko <akame.dante13@gmail.com>
2024-03-11 22:44:50 +0200

    Optimized for loop by decreasing iterates quantity

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -6,13 +6,11 @@ const val ARRAY_SIZE = 6
 
 fun main() {
     val array = Array(ARRAY_SIZE) { FigureSupplier().getDefaultFigure() }
-    for (i in 0 until array.lastIndex) {
-        if (i < array.size / 2) {
-            array[i] = FigureSupplier().getRandomFigure()
-        }
+    for (i in 0 until array.size / 2) {
+        array[i] = FigureSupplier().getRandomFigure()
     }
 
-    array.forEachIndexed {
-        index, figure -> println("Figure ${index + 1}: ${figure.getDescription()}")
+    array.forEachIndexed { index, figure ->
+        println("Figure ${index + 1}: ${figure.getDescription()}")
     }
 }
