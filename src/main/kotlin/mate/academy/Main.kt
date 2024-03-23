35a6603e708cc1b81d1da6aa8695b55aa3345a39
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2024-03-23 12:04:41 +0200

    3 version

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -3,17 +3,19 @@ package mate.academy
 import mate.academy.model.Figure
 import mate.academy.service.FigureSupplier
 
-const val FIRST_PART = 2
-const val SECOND_PART = 5
+const val START_FIRST_PART = 0
+const val END_FIRST_PART = 2
+const val START_SECOND_PART = 3
+const val END_SECOND_PART = 5
 fun main() {
     val figureList: MutableList<Figure> = mutableListOf()
     val figureSupplier = FigureSupplier()
-    for(i in 0..FIRST_PART) {
+    for(i in START_FIRST_PART..END_FIRST_PART) {
         val figure = figureSupplier.getRandomFigure()
         figure.behavior()
         figureList.add(figure)
     }
-    for (i in 3..SECOND_PART) {
+    for (i in START_SECOND_PART..END_SECOND_PART) {
         val figure = figureSupplier.getDefaultFigure()
         figure.behavior()
         figureList.add(figure)
