413055ffd5f17cd993e0bfb06666691cb9f0926d
khorkovenko <khorkovenko>
2024-04-01 10:57:24 +0300

    add O. Kapysniak comments

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -2,51 +2,75 @@ package mate.academy
 
 import mate.academy.model.Color
 import mate.academy.service.ColorSupplier
+import java.util.*
 import kotlin.math.PI
 import kotlin.math.pow
 import kotlin.random.Random
+
+private const val HALF = 0.5
+private const val MIN_RANDOM_VALUE = 1.0
+private const val MAX_RANDOM_VALUE = 10.0
+private const val DEFAULT_CIRCLE_RADIUS = 10.0
+private const val FIGURES_COUNT = 6
+private const val RANDOM_FIGURES_COUNT = 3
 
 abstract class Figure(val color: Color) {
     abstract fun area(): Double
     abstract fun draw()
 }
 
-class Square(private val side: Double, color: Color) : Figure(color) {
+class Square(
+    private val side: Double,
+    color: Color
+) : Figure(color) {
     override fun area(): Double = side * side
 
     override fun draw() {
-        println("Figure: square, area: ${area()} sq. units, side: $side units, color: $color")
+        println("""Figure: square, area: ${area()} sq. units,
+            side: $side units, color: $color""".trimMargin())
     }
 }
 
-class Rectangle(private val length: Double, private val width: Double, color: Color) : Figure(color) {
+class Rectangle(
+    private val length: Double,
+    private val width: Double,
+    color: Color
+) : Figure(color) {
     override fun area(): Double = length * width
 
     override fun draw() {
-        println("Figure: rectangle, area: ${area()} sq. units, length:"
-                + " $length units, width: $width units, color: $color")
+        println("""Figure: rectangle, area: ${area()} sq. units, 
+            length: $length units, 
+            width: $width units, 
+            color: $color""".trimMargin())
     }
 }
 
-class RightTriangle(private val firstLeg: Double, private val secondLeg: Double, color: Color) : Figure(color) {
-    companion object {
-        const val HALF = 0.5
-    }
-
+class RightTriangle(
+    private val firstLeg: Double,
+    private val secondLeg: Double,
+    color: Color
+) : Figure(color) {
     override fun area(): Double = HALF * firstLeg * secondLeg
 
     override fun draw() {
-        println("Figure: triangle, area: ${area()} sq. units, firstLeg: "
-                + "$firstLeg units, secondLeg: $secondLeg units, color: $color")
+        println("""Figure: triangle, area: ${area()} sq. units, 
+            firstLeg: $firstLeg units, 
+            secondLeg: $secondLeg units, 
+            color: $color""".trimMargin())
     }
 }
 
-
-class Circle(private val radius: Double, color: Color) : Figure(color) {
+class Circle(
+    private val radius: Double,
+    color: Color
+) : Figure(color) {
     override fun area(): Double = PI * radius.pow(2)
 
     override fun draw() {
-        println("Figure: circle, area: ${area()} sq. units, radius: $radius units, color: $color")
+        println("""Figure: circle, area: ${area()} sq. units, 
+            radius: $radius units, 
+            color: $color""".trimMargin())
     }
 }
 
@@ -56,31 +80,34 @@ class IsoscelesTrapezoid(
     private val height: Double,
     color: Color
 ) : Figure(color) {
-    companion object {
-        const val HALF = 0.5
-    }
-
     override fun area(): Double = HALF * (upperBase + lowerBase) * height
 
     override fun draw() {
-        println("Figure: trapezoid, area: ${area()} sq. units, upperBase: $upperBase units, "
-                + "lowerBase: $lowerBase units, height: $height units, color: $color")
+        println("""Figure: trapezoid, area: ${area()} sq. units, 
+             upperBase: $upperBase units, 
+             lowerBase: $lowerBase units, 
+             height: $height units, 
+             color: $color""".trimMargin())
     }
 }
 
 class FigureSupplier {
     private val colorSupplier = ColorSupplier()
-    companion object {
-        const val MIN_RANDOM_VALUE = 1.0
-        const val MAX_RANDOM_VALUE = 10.0
-        const val DEFAULT_CIRCLE_RADIUS = 10.0
+    private val figureTypesStack = Stack<String>()
+
+    init {
+        figureTypesStack.push("Square")
+        figureTypesStack.push("Rectangle")
+        figureTypesStack.push("RightTriangle")
+        figureTypesStack.push("Circle")
+        figureTypesStack.push("IsoscelesTrapezoid")
     }
 
     fun getRandomFigure(): Figure {
         val color = colorSupplier.getRandomColor()
-        return when (val figureType =
-            listOf("Square", "Rectangle", "RightTriangle", "Circle", "IsoscelesTrapezoid").random()) {
+        return when (val figureType = figureTypesStack.pop()) {
             "Square" -> Square(Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE), color)
+
             "Rectangle" -> Rectangle(
                 Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE),
                 Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE),
@@ -94,6 +121,7 @@ class FigureSupplier {
             )
 
             "Circle" -> Circle(Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE), color)
+
             "IsoscelesTrapezoid" -> IsoscelesTrapezoid(
                 Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE),
                 Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE),
@@ -110,13 +138,10 @@ class FigureSupplier {
     }
 }
 
-const val FIGURES_COUNT = 6
-const val RANDOM_FIGURES_COUNT = 3
-
 fun main() {
     val figureSupplier = FigureSupplier()
     val figures =
-        Array<Figure>(FIGURES_COUNT) {
+        Array(FIGURES_COUNT) {
             if (it < RANDOM_FIGURES_COUNT)
                 figureSupplier.getRandomFigure()
             else
