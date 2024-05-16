cf9c1163b6f625861d75cdd1ee0c2f2fb3da258d
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-05-16 15:10:59 +0300

    Main.kt

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,5 +1,19 @@
 package mate.academy
 
+import mate.academy.model.*
+import mate.academy.service.FigureSupplier
+
 fun main() {
-    // draw all figures here
+    val figureSupplier = FigureSupplier()
+    val figures = arrayOfNulls<Figure>(6)
+
+    for (i in 0 until figures.size / 2) {
+        figures[i] = figureSupplier.getRandomFigure()
+    }
+
+    for (i in figures.size / 2 until figures.size) {
+        figures[i] = figureSupplier.getDefaultFigure()
+    }
+
+    figures.forEach { it?.draw() }
 }
