61bd2e2d6a85fc394d6d3eb9cf5190cbd88f5ad7
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-05-16 14:52:04 +0300

    Delete src/main/kotlin/mate/academy/Rectangle.kt

--- src/main/kotlin/mate/academy/Rectangle.kt
+++
@@ -1,6 +0,0 @@
-package mate.academy.model
-
-class Rectangle(override val color: Color, private val width: Double, private val height: Double) : Figure {
-    override fun area() = width * height
-    override fun draw() = println("Figure: rectangle, area: ${area()} sq. units, width: $width units, height: $height units, color: $color")
-}
