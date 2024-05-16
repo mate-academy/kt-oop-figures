97d370ef8adf126923e82a3a358824ba1a3fda2d
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-05-16 15:16:23 +0300

    FigureSupplier.kt

--- src/main/kotlin/mate/academy/service/FigureSupplier.kt
+++ src/main/kotlin/mate/academy/service/FigureSupplier.kt
@@ -6,16 +6,27 @@ import kotlin.random.Random
 class FigureSupplier {
     private val colorSupplier = ColorSupplier()
 
+    companion object {
+        private const val MAX_RANDOM_VALUE = 10.0
+        private const val MIN_RANDOM_VALUE = 1.0
+        private const val DEFAULT_CIRCLE_RADIUS = 10.0
+        private const val NUM_FIGURE_TYPES = 5
+    }
+
     fun getRandomFigure(): Figure {
         val color = colorSupplier.getRandomColor()
-        return when (Random.nextInt(5)) {
-            0 -> Square(color, Random.nextDouble(1.0, 10.0))
-            1 -> Rectangle(color, Random.nextDouble(1.0, 10.0), Random.nextDouble(1.0, 10.0))
-            2 -> RightTriangle(color, Random.nextDouble(1.0, 10.0), Random.nextDouble(1.0, 10.0))
-            3 -> Circle(color, Random.nextDouble(1.0, 10.0))
-            else -> IsoscelesTrapezoid(color, Random.nextDouble(1.0, 10.0), Random.nextDouble(1.0, 10.0), Random.nextDouble(1.0, 10.0))
+        return when (Random.nextInt(NUM_FIGURE_TYPES)) {
+            0 -> Square(color, Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE))
+            1 -> Rectangle(color, Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE),
+                           Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE))
+            2 -> RightTriangle(color, Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE),
+                               Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE))
+            3 -> Circle(color, Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE))
+            else -> IsoscelesTrapezoid(color, Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE),
+                                       Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE),
+                                       Random.nextDouble(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE))
         }
     }
 
-    fun getDefaultFigure(): Figure = Circle(Color.WHITE, 10.0)
+    fun getDefaultFigure(): Figure = Circle(Color.WHITE, DEFAULT_CIRCLE_RADIUS)
 }
