66c994014ee202fd16464f153b00858cd4432a3c
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-05-16 14:52:46 +0300

    Delete src/main/kotlin/mate/academy/Circle.kt

--- src/main/kotlin/mate/academy/Circle.kt
+++
@@ -1,6 +0,0 @@
-package mate.academy.model
-
-class Circle(override val color: Color, private val radius: Double) : Figure {
-    override fun area() = Math.PI * radius * radius
-    override fun draw() = println("Figure: circle, area: ${area()} sq. units, radius: $radius units, color: $color")
-}
