1eebe07b03e33abde2053382eae189a74a57882a
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-05-16 14:51:55 +0300

    Delete src/main/kotlin/mate/academy/RightTriangle.kt

--- src/main/kotlin/mate/academy/RightTriangle.kt
+++
@@ -1,6 +0,0 @@
-package mate.academy.model
-
-class RightTriangle(override val color: Color, private val firstLeg: Double, private val secondLeg: Double) : Figure {
-    override fun area() = 0.5 * firstLeg * secondLeg
-    override fun draw() = println("Figure: triangle, area: ${area()} sq. units, firstLeg: $firstLeg units, secondLeg: $secondLeg units, color: $color")
-}
