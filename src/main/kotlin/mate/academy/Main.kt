a3a46cdf2db601d00492eff331cfff7eb528bac0
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-05-16 15:18:38 +0300

    Main.kt

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -6,7 +6,8 @@ import mate.academy.service.FigureSupplier
 
 fun main() {
     val figureSupplier = FigureSupplier()
-    val figures = arrayOfNulls<Figure>(6)
+    const val NUM_FIGURES = 6
+    val figures = arrayOfNulls<Figure>(NUM_FIGURES)
 
     for (i in 0 until figures.size / 2) {
         figures[i] = figureSupplier.getRandomFigure()
