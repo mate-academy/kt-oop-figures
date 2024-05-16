4f1e09af006406998a19ec1bfe07db06ae2a0b39
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-05-16 14:45:00 +0300

    Rectangle.kt

---
+++ src/main/kotlin/mate/academy/Rectangle.kt
@@ -0,0 +1,6 @@
+package mate.academy.model
+
+class Rectangle(override val color: Color, private val width: Double, private val height: Double) : Figure {
+    override fun area() = width * height
+    override fun draw() = println("Figure: rectangle, area: ${area()} sq. units, width: $width units, height: $height units, color: $color")
+}
