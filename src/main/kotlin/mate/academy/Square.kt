1517c9cc8dd0ba2b80ee0eb8a5307911a16c6295
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-05-16 14:51:44 +0300

    Delete src/main/kotlin/mate/academy/Square.kt

--- src/main/kotlin/mate/academy/Square.kt
+++
@@ -1,6 +0,0 @@
-package mate.academy.model
-
-class Square(override val color: Color, private val side: Double) : Figure {
-    override fun area() = side * side
-    override fun draw() = println("Figure: square, area: ${area()} sq. units, side: $side units, color: $color")
-}
