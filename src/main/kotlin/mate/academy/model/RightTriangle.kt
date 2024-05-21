fccd2159224e82c7129ccbf1d8d20dba5ddcc1a6
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-05-21 10:02:27 +0300

    RightTriangle.kt

--- src/main/kotlin/mate/academy/model/RightTriangle.kt
+++ src/main/kotlin/mate/academy/model/RightTriangle.kt
@@ -9,4 +9,12 @@ class RightTriangle(
         private const val DEFAULT_BASE = 3.0
         private const val DEFAULT_HEIGHT = 4.0
     }
+
+    override fun area(): Double {
+        return (base * height) / 2.0
+    }
+
+    override fun draw() {
+        println("Drawing a right triangle with base $base, height $height, and color $color")
+    }
 }
