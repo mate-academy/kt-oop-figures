bed27a8b9a167ce994298425387845e5c4497a2c
igortrohimcuk <trokhymchuk.ihor.dev@gmail.com>
2024-07-02 09:37:09 +0300

    Move create random figure logic from enum to FigureSup

--- src/main/kotlin/mate/academy/service/FigureSupplier.kt
+++ src/main/kotlin/mate/academy/service/FigureSupplier.kt
@@ -1,14 +1,35 @@
 package mate.academy.service
 
 import mate.academy.figure.Circle
+import mate.academy.figure.Figure
+import mate.academy.figure.IsoscelesTrapezoid
+import mate.academy.figure.Rectangle
+import mate.academy.figure.RightTriangle
+import mate.academy.figure.Square
 import mate.academy.model.Color
+import kotlin.random.Random
 
 private const val DEFAULT_RADIUS = 10.0
+
+private const val MAX_RANDOM_VALUE = 20.0
+
 
 class FigureSupplier {
     private val colorSupplier = ColorSupplier()
 
-    fun getRandomFigure() = RandomFigureType.values().random().createFigure(colorSupplier)
+    fun getRandomFigure() : Figure {
+        return when(FigureType.values().random()) {
+            FigureType.CIRCLE -> Circle(colorSupplier.getRandomColor(), Random.nextDouble(MAX_RANDOM_VALUE))
+            FigureType.ISOSCELES_TRAPEZOID -> IsoscelesTrapezoid(colorSupplier.getRandomColor(),
+                Random.nextDouble(MAX_RANDOM_VALUE), Random.nextDouble(MAX_RANDOM_VALUE),
+                Random.nextDouble(MAX_RANDOM_VALUE))
+            FigureType.RECTANGLE -> Rectangle(colorSupplier.getRandomColor(),
+                Random.nextDouble(MAX_RANDOM_VALUE), Random.nextDouble(MAX_RANDOM_VALUE))
+            FigureType.RIGHT_TRIANGLE -> RightTriangle(colorSupplier.getRandomColor(),
+                Random.nextDouble(MAX_RANDOM_VALUE), Random.nextDouble(MAX_RANDOM_VALUE))
+            FigureType.SQUARE -> Square(colorSupplier.getRandomColor(), Random.nextDouble(MAX_RANDOM_VALUE))
+        }
+    }
 
     fun getDefaultFigure() = Circle(Color.WHITE, DEFAULT_RADIUS)
 }
---
+++ src/main/kotlin/mate/academy/service/FigureType.kt
@@ -0,0 +1,9 @@
+package mate.academy.service
+
+enum class FigureType {
+    CIRCLE,
+    ISOSCELES_TRAPEZOID,
+    RECTANGLE,
+    RIGHT_TRIANGLE,
+    SQUARE,
+}
