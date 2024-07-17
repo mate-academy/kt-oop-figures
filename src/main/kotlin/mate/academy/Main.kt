9ffd167c42896a9d1518e3d7e0668e3b7384d05d
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-07-17 17:45:52 +0300

    refactor [kotlin]: remove code duplication

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -2,11 +2,16 @@ package mate.academy
 
 import mate.academy.service.FigureSupplier
 
-private const val ITEMS_QUANTITY = 3
+private const val DEFAULT_FIGURE_START_INDEX = 3
+private const val ITEMS_QUANTITY = 6
 
 fun main() {
     val figureSupplier = FigureSupplier()
 
-    repeat(ITEMS_QUANTITY) { println(figureSupplier.getRandomFigure().getArea()) }
-    repeat(ITEMS_QUANTITY) { println(figureSupplier.getDefaultFigure().getArea()) }
+    repeat(ITEMS_QUANTITY) {
+        println(
+            if (it < DEFAULT_FIGURE_START_INDEX) figureSupplier.getRandomFigure().getArea()
+            else figureSupplier.getDefaultFigure().getArea()
+        )
+    }
 }
