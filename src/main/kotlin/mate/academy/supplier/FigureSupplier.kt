5af801e61ae4477caeaa5882b2d33d7241f08bfc
John <pandastars535@gmail.com>
2024-08-11 08:47:18 +0300

    Solution

--- src/main/kotlin/mate/academy/supplier/FigureSupplier.kt
+++ src/main/kotlin/mate/academy/supplier/FigureSupplier.kt
@@ -1,6 +1,11 @@
 package mate.academy.supplier
 
-import mate.academy.model.model.*
+import mate.academy.model.model.Circle
+import mate.academy.model.model.Figure
+import mate.academy.model.model.IsoscelesTrapezoid
+import mate.academy.model.model.Rectangle
+import mate.academy.model.model.RightTriangle
+import mate.academy.model.model.Square
 import kotlin.random.Random
 
 private const val MAX_SIZE: Double = 10.0
@@ -8,6 +13,11 @@ private const val MIX_SIZE: Double = 1.0
 private const val DEFAULT_SIZE: Double = 10.0
 private const val FIGURES_NUMBER = 5
 private const val DEFAULT_COLOR = "WHITE"
+private const val SQUARE_INDEX = 0
+private const val RECTANGLE_INDEX = 1
+private const val CIRCLE_INDEX = 2
+private const val RIGHT_TRIANGLE_INDEX = 3
+
 
 class FigureSupplier {
 
@@ -17,22 +27,22 @@ class FigureSupplier {
     fun getRandomFigure(): Figure {
         return when (random.nextInt(FIGURES_NUMBER)) {
 
-            0 -> Square(
+            SQUARE_INDEX -> Square(
                 color = colorSupplier.getRandomColor(), side = random.nextDouble(MIX_SIZE, MAX_SIZE)
             )
 
-            1 -> Rectangle(
+            RECTANGLE_INDEX -> Rectangle(
                 color = colorSupplier.getRandomColor(),
                 firstSide = random.nextDouble(MIX_SIZE, MAX_SIZE),
                 secondSide = random.nextDouble(MIX_SIZE, MAX_SIZE)
             )
 
-            2 -> Circle(
+            CIRCLE_INDEX -> Circle(
                 color = colorSupplier.getRandomColor(),
-                radius = random.nextDouble(MAX_SIZE)
+                radius = random.nextDouble(MIX_SIZE,MAX_SIZE)
             )
 
-            3 -> RightTriangle(
+            RIGHT_TRIANGLE_INDEX -> RightTriangle(
                 color = colorSupplier.getRandomColor(),
                 firstLeg = random.nextDouble(MIX_SIZE, MAX_SIZE),
                 secondLeg = random.nextDouble(MIX_SIZE, MAX_SIZE)
