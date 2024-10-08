cd8ecbbafd888054b11b38c50b275686ada59b29
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-10-08 14:32:19 +0300

    IsoscelesTrapezoid.kt

--- src/main/kotlin/mate/academy/model/IsoscelesTrapezoid.kt
+++ src/main/kotlin/mate/academy/model/IsoscelesTrapezoid.kt
@@ -1,17 +1,19 @@
 package mate.academy.model
 
 class IsoscelesTrapezoid(
-    color: Color,
+    private val color: Color,
     private val base1: Double,
     private val base2: Double,
     private val height: Double
-) : Figure(color) {
+) : Figure() {
     override fun area(): Double = 0.5 * (base1 + base2) * height
 
     override fun draw() {
         println(
-            "Figure: isosceles trapezoid, area: ${area()} sq. units, " +
-            "base1: $base1 units, base2: $base2 units, height: $height units, color: $color"
+            """
+            Figure: isosceles trapezoid, area: ${area()} sq. units, 
+            base1: $base1 units, base2: $base2 units, height: $height units, color: $color
+            """.trimIndent()
         )
     }
 }
