99e8d6e7b2d71c750bdce91830b9b89008bdcb14
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-10-08 08:03:45 +0300

    RightTriangle.kt

--- src/main/kotlin/mate/academy/model/RightTriangle.kt
+++ src/main/kotlin/mate/academy/model/RightTriangle.kt
@@ -1,9 +1,16 @@
 package mate.academy.model
 
-class RightTriangle(color: Color, private val firstLeg: Double, private val secondLeg: Double) : Figure(color) {
+class RightTriangle(
+    color: Color,
+    private val firstLeg: Double,
+    private val secondLeg: Double
+) : Figure(color) {
     override fun area(): Double = 0.5 * firstLeg * secondLeg
 
     override fun draw() {
-        println("Figure: right triangle, area: ${area()} sq. units, firstLeg: $firstLeg units, secondLeg: $secondLeg units, color: $color")
+        println(
+            "Figure: right triangle, area: ${area()} sq. units, " +
+            "firstLeg: $firstLeg units, secondLeg: $secondLeg units, color: $color"
+        )
     }
 }
