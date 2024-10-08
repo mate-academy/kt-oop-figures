2e74b38aaf8fc52bf0c138280d9b63b2017fee0b
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-10-08 07:55:45 +0300

    Circle.kt

---
+++ src/main/kotlin/mate/academy/model/Circle.kt
@@ -0,0 +1,11 @@
+package mate.academy.model
+
+import kotlin.math.PI
+
+class Circle(color: Color, private val radius: Double) : Figure(color) {
+    override fun area(): Double = PI * radius * radius
+
+    override fun draw() {
+        println("Figure: circle, area: ${area()} sq. units, radius: $radius units, color: $color")
+    }
+}
