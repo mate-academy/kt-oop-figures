7566dfd9e29d7774f903f8c0a2c4af3659b9adb1
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-10-08 14:41:59 +0300

    FigureSupplier.kt

--- src/main/kotlin/mate/academy/service/FigureSupplier.kt
+++ src/main/kotlin/mate/academy/service/FigureSupplier.kt
@@ -1,6 +1,13 @@
 package mate.academy.service
 
-import mate.academy.model.*
+import mate.academy.model.Circle
+import mate.academy.model.Color
+import mate.academy.model.Figure
+import mate.academy.model.FigureType
+import mate.academy.model.IsoscelesTrapezoid
+import mate.academy.model.Rectangle
+import mate.academy.model.RightTriangle
+import mate.academy.model.Square
 import kotlin.random.Random
 
 class FigureSupplier(private val colorSupplier: ColorSupplier) {
