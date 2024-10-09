09ffdddbaf7d80b524844d22696f1015bfb40f16
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-10-09 12:11:07 +0300

    Main.kt

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -6,10 +6,10 @@ import mate.academy.service.FigureSupplier
 fun main() {
     val colorSupplier = ColorSupplier()
     val figureSupplier = FigureSupplier(colorSupplier)
-    val numberOfFigures = 6
+    val NUMBER_OF_FIGURES = 6
 
-    repeat(numberOfFigures) { index ->
-        val figure = if (index < numberOfFigures / 2) {
+    repeat(NUMBER_OF_FIGURES) { index ->
+        val figure = if (index < NUMBER_OF_FIGURES / 2) {
             figureSupplier.getRandomFigure()
         } else {
             figureSupplier.getDefaultFigure()
