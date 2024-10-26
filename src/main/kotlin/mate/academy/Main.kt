e07d65bd751250d9097b3689c1ebbc1724f67ab4
Danylo Kishchenko <amfich@gmail.com>
2024-10-26 19:50:57 +0300

    removed redundant comments

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -5,13 +5,10 @@ import mate.academy.service.FigureSupplier
 const val ARRAY_SIZE = 6
 
 fun main() {
-    // draw all figures here
     val figureSupplier = FigureSupplier()
     val figures = Array(ARRAY_SIZE) {
         if (it < ARRAY_SIZE / 2) figureSupplier.getRandomFigure()
         else figureSupplier.getDefaultFigure()
     }
-
-    // Display each figure
     figures.forEach { it.draw() }
 }
