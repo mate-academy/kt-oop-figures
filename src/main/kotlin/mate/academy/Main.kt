9da666a7bdae0d2aeab4dbbbf1a58364a0a4c178
Artem Klishch <artemklishch@gmail.com>
2024-11-25 18:33:52 +0200

    fix: fixed the loop condition

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -7,7 +7,7 @@ const val FIGURES_NUMBER_FOR_RENDER = 10
 fun main() {
     val figureSupplier = FigureSupplier()
     val middleIndex = FIGURES_NUMBER_FOR_RENDER / 2
-    for (i in 0..FIGURES_NUMBER_FOR_RENDER) {
+    for (i in 0 until FIGURES_NUMBER_FOR_RENDER) {
         val figure = if (i < middleIndex) {
             figureSupplier.getRandomFigure()
         } else {
