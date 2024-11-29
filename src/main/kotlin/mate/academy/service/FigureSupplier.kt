f69e3a1b399fe7159773b8bee362901d50612f41
Anatolii Nazarov <anatoly_n@ukr.net>
2024-11-29 14:41:59 +0200

    For check

--- src/main/kotlin/mate/academy/service/FigureSupplier.kt
+++ src/main/kotlin/mate/academy/service/FigureSupplier.kt
@@ -11,12 +11,13 @@ import kotlin.random.Random
 
 const val MIN_RANDOM_VALUE = 1
 const val MAX_RANDOM_VALUE = 10
+const val NUM_FIGURE_TYPES = 5
 
 class FigureSupplier {
 
     fun getRandomFigure(): Figure {
         val colorSupplier = ColorSupplier()
-        return when (Random.nextInt(5)) {
+        return when (Random.nextInt(NUM_FIGURE_TYPES)) {
             0 -> Square((MIN_RANDOM_VALUE..MAX_RANDOM_VALUE).random().toDouble(),
                 colorSupplier.getRandomColor())
             1 -> Rectangle((MIN_RANDOM_VALUE..MAX_RANDOM_VALUE).random().toDouble(),
