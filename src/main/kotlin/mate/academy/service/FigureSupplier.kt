9eba9e063e556c2ad3134752c4f4f614e50d07df
OD <sdenisenko055@gmail.com>
2025-03-12 10:22:14 +0200

    fix

--- src/main/kotlin/mate/academy/service/FigureSupplier.kt
+++ src/main/kotlin/mate/academy/service/FigureSupplier.kt
@@ -1,6 +1,11 @@
 package mate.academy.service
 
-import mate.academy.model.*
+import mate.academy.model.IsoscelesTrapezoid
+import mate.academy.model.Circle
+import mate.academy.model.Color
+import mate.academy.model.Figure
+import mate.academy.model.RightTriangle
+import mate.academy.model.Square
 import kotlin.random.Random.Default.nextDouble
 import kotlin.random.Random.Default.nextInt
 
