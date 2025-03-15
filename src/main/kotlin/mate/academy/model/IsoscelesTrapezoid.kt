9874e9435872d9ad427738c8d9f0568834e77e3d
Roman Voynahiy <romanvoynahiy@gmail.com>
2025-03-15 15:50:09 +0200

    Fix formula for calculating are in IsoscelesTrapezoid's getArea() function.

--- src/main/kotlin/mate/academy/model/IsoscelesTrapezoid.kt
+++ src/main/kotlin/mate/academy/model/IsoscelesTrapezoid.kt
@@ -4,7 +4,7 @@ const val AREA_MULTIPLIER = 0.5
 
 class IsoscelesTrapezoid(color: String, val upperBase: Double, val bottomBase: Double,
                          val height : Double) : Figure(color) {
-    override fun getArea(): Double = AREA_MULTIPLIER * (upperBase * bottomBase) * height
+    override fun getArea(): Double = AREA_MULTIPLIER * (upperBase + bottomBase) * height
 
     override fun draw() = println("Figure: isosceles trapezoid, area: ${getArea()} sq. units,"
             + " upper base: $upperBase units, bottom base: $bottomBase units,"
